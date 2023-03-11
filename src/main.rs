00c405a4bc5353898275db7672dce63bc89db7d4
Amos Wenger <amoswenger@gmail.com>
2023-03-11 20:31:23 +0100

    Superblock type

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
 use byteorder::{LittleEndian, ReadBytesExt};
+use custom_debug::Debug as CustomDebug;
 use positioned_io::{Cursor, ReadAt, Slice};
 use std::fs::OpenOptions;
+
+#[derive(CustomDebug)]
+struct Superblock {
+    #[debug(format = "{:x}")]
+    magic: u16,
+    block_size: u64,
+    blocks_per_group: u64,
+    inodes_per_group: u64,
+    inode_size: u64,
+}
+
+impl Superblock {
+    fn new(dev: &dyn ReadAt) -> color_eyre::Result<Self> {
+        let r = Reader::new(Slice::new(dev, 1024, None));
+        // note: we're casting a few fields to `u64` now.
+        // this will save us a bunch of grief later.
+        Ok(Self {
+            magic: r.u16(0x38)?,
+            block_size: (2u32.pow(10 + r.u32(0x18)?)) as u64,
+            blocks_per_group: r.u32(0x20)? as u64,
+            inodes_per_group: r.u32(0x28)? as u64,
+            inode_size: r.u16(0x58)? as u64,
+        })
+    }
+}
 
 struct Reader<IO> {
     inner: IO,
@@ -23,22 +49,11 @@ impl<IO: ReadAt> Reader<IO> {
 }
 
 fn main() -> color_eyre::Result<()> {
+    // open our ext4 partition, READ-ONLY.
     let file = OpenOptions::new().read(true).open("/dev/sda3")?;
 
-    let r = Reader::new(Slice::new(file, 1024, None));
-
-    let magic = r.u16(0x38)?;
-    println!("magic = {magic:x}");
-
-    let n = r.u32(0x18)?;
-    let block_size = 1 << (10 + n);
-    println!("block_size = {block_size}");
-
-    let bpg = r.u32(0x20)?;
-    println!("blocks per group = {bpg}");
-
-    let ipg = r.u32(0x28)?;
-    println!("inodes per group = {ipg}");
+    let sb = Superblock::new(&file)?;
+    println!("{sb:#?}");
 
     Ok(())
 }
