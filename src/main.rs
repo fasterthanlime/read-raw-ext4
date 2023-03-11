d6ba1311b9b25f1a5bcb7365ec1fee768474fd12
Amos Wenger <amoswenger@gmail.com>
2023-03-11 20:26:42 +0100

    dump inode per group

--- src/main.rs
+++ src/main.rs
@@ -28,11 +28,17 @@ fn main() -> color_eyre::Result<()> {
     let r = Reader::new(Slice::new(file, 1024, None));
 
     let magic = r.u16(0x38)?;
-    println!("magic = {:x}", magic);
+    println!("magic = {magic:x}");
 
     let n = r.u32(0x18)?;
     let block_size = 1 << (10 + n);
-    println!("block_size = {}", block_size);
+    println!("block_size = {block_size}");
+
+    let bpg = r.u32(0x20)?;
+    println!("blocks per group = {bpg}");
+
+    let ipg = r.u32(0x28)?;
+    println!("inodes per group = {ipg}");
 
     Ok(())
 }
